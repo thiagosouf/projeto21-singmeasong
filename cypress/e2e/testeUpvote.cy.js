7746f406d8c0dd2b3289e2b719f7c6d86abd77b3
thiagosouf <thiagosouf@gmail.com>
2022-07-29 14:56:05 -0300

    testes front cypress

---
+++ cypress/e2e/testeUpvote.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+
+describe("testando Upvote", () => {
+    it("Upvote",() => {
+
+    cy.visit("http://localhost:3000")
+    it("dando like", () => {
+
+        await cy.get(':nth-child(4) > :nth-child(1)').should("be.visible");
+
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path')
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path').click()
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path').click()
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path').click()
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path').click()
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path').click()
+        cy.get(':nth-child(4) > :nth-child(3) > :nth-child(1) > path').click()
+    
+    
+        
+    })
+    })
+})
